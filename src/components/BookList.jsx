b9f3f239ebbd34f949b2ef604c823e8d6944f5d9
sperilla <smperilla@gmail.com>
2023-11-13 14:50:34 -0500

    created favorites functionality

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import '../App';
 
 const BookList = () => {
   return (
-    <div className='book-list' >BookList</div>
+    <div className='book-list' >BookList
+     <Link to="/search" className="button-link">Search for Books</Link>
+     <Link to="/favorites" className="button-link">View Favorites</Link>
+    </div>
   )
 }
 

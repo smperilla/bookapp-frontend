a48307d54240027e4d260d4f69a46da832ea6016
sperilla <smperilla@gmail.com>
2023-11-13 13:44:39 -0500

    moved folders

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        
+       <div>
+        <h1>React Books App</h1>
+       </div>
+       <div>
+        Your Favorite
+       </div>
+        </div>
+  )
+}
+
+export default Navbar

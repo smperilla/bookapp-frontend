e11a6847ab93872797edd4ab1ddc7d9f0e478c30
sperilla <smperilla@gmail.com>
2023-11-13 14:13:59 -0500

    more components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import BookList from "./components/BookList";
+import BookDetails from "./components/BookDetails";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Favorites from "./components/Favorites";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<BookList />} />
-        <Route path="/book/:id" element={<BookList />} />
+        <Route path="/book/:id" element={<BookDetails />} />
+        <Route path="/favorites" elements={<Favorites />} />
       </Routes>
     </div>
   );
